7d27ec1ca33cfef3060d23ea5c7d542a96e64533
Timothé Bertrand <timothe.bertrand@arke.fr>
2022-01-04 19:37:51 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="loading"/>
+</template>
+
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+     this.$router.push("/neofetch")
+    }, 2000)
+  }
+}
+
+
+</script>
+
+<style>
+#loading {
+  background-color: black;
+
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
